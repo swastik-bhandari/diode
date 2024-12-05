e011b32be78d1b3537ed710cd81e1f3c13f87952
swastik-bhandari <swastikbhandari2006@gmail.com>
2024-12-05 12:23:54 +0545

    skdfskldfl

---
+++ js/login-login.js
@@ -0,0 +1,9 @@
+        // JavaScript to toggle password visibility
+        function togglePassword() {
+            const passwordField = document.getElementById('password');
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+            } else {
+                passwordField.type = 'password';
+            }
+        }
--- php/login-login.php
+++ php/login-login.php
@@ -7,51 +7,60 @@
     <link rel="stylesheet" href="../css/login-login-styles.css">
 </head>
 <body>
-<?php 
-    $username='';
-    $password='';
-    if($_SERVER['REQUEST_METHOD']==='POST') {
-        function test_input($data) {
-            $data=stripslashes($data);
-            $data=trim($data);
-            $data=htmlspecialchars($data);
-            return $data;
+    <?php 
+        $username = '';
+        $password = '';
+        $error_message = '';
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            function test_input($data) {
+                $data = stripslashes($data);
+                $data = trim($data);
+                $data = htmlspecialchars($data);
+                return $data;
+            }
+    
+            $username = test_input($_POST['username']);
+            $password = test_input($_POST['password']);
+    
+            if (!preg_match('/[A-Z]/', $password) || !preg_match('/[\W_]/', $password)) {
+                $error_message = "*atleast one capital and special char";
+            }
         }
-
-    $username=test_input($_POST['username']);
-    $password=test_input($_POST['password']);
-}
     ?>
-
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
-    <div id="login-container">
-        <div><h2>Login</h2></div>
-
-        <div class="input-group">
-            <label for="username">Username</label>
-            <input type="text" id="username" name ="username" placeholder="Enter your username" required>
+    
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <div id="login-container">
+            <div><h2>Login</h2></div>
+    
+            <div class="input-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Enter your username" required value="<?php echo $username; ?>">
+            </div>
+    
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+                <span class="error"><?php echo $error_message; ?></span>
+            </div>
+    
+            <div class="input-group" id="showpassword">
+                <div>
+                    <label for="show-password">Show Password</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="show-password" onclick="togglePassword()">
+                </div>
+            </div>
+    
+            <div class="input-group">
+                <button class="button" id="login-btn" type="submit">Log In</button>
+            </div>
         </div>
-
-        <div class="input-group">
-            <label for="password">Password</label>
-            <input type="password" id="password" name ="password" placeholder="Enter your password" required> <span class ="error"> <?php 
-    if(!preg_match('/[A-Z]/',$password) || !preg_match('/[\W_]/' ,$password)) {
-        echo"*atleast one capital letter and one special character";
-    }
-    ?></span>
-        </div>
-
-        <div class="input-group" id = "showpassword">
-            <div><label for="show-password">Show Password</label></div>
-            <div><input type="checkbox" id="show-password"> </div>
-        </div>
-
-        <div class="input-group">
-            <button class="button" id="login-btn">Log In</button>
-        </div>
-
-    </div>
     </form>
-    <script src="../js/login-script.js"></script>
-</body>
-</html>
+    
+    <script src="../js/login-login.js">
+
+    </script>
+    </body>
+    </html>
