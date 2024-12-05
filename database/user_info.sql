c327b9f31183e9018677bc02683dfa5049224042
Loopfelloff <abhiyanregmi@gmail.com>
2024-12-05 13:02:20 +0545

    removed old database added new database lol

--- database/user_info.sql
+++ database/user_info.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2024 at 07:56 AM
+-- Generation Time: Dec 05, 2024 at 08:16 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -24,22 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `logininfo`
+-- Table structure for table `user_info`
 --
 
-CREATE TABLE `logininfo` (
+CREATE TABLE `user_info` (
   `username` varchar(50) NOT NULL,
   `password` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`username`, `password`) VALUES
+('adarshfuckboi23', '$2y$10$.p.zgW5eiT3QBLIc.IQLEujqb3BITLuSsiTOo9IBpN5//yjM3inxG'),
+('dfasdsdfsdfsdfsd', '$2y$10$bjxqDoCE.3XuBidJPcSFROLTpE53mzgQUXOUh7uY9qyAalchi6HHe'),
+('dsfdfsadfdfasdf454', '$2y$10$yw1oVId.FjCPw1ty0vOBHe6ImWWM8JUWUrJgM17eS3FogqBAHVpuu');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `logininfo`
+-- Indexes for table `user_info`
 --
-ALTER TABLE `logininfo`
+ALTER TABLE `user_info`
   ADD PRIMARY KEY (`username`);
 COMMIT;
 
