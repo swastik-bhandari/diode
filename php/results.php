0f6494f9cb3697094dea12e95cae41a2dcc89704
Adarsha Ghimire <adarshghimire2005@gmail.com>
2024-12-05 23:35:29 +0545

    added results page

---
+++ php/results.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Find Your Mentor</title>
+        <link rel="stylesheet" href="../css/results-style.css">
+    </head>
+    <body>
+        <div id = "header"><h1>Search Results</h1></div>
+        <div class="results">
+            <div class="mentor-card">
+                <div class="mentor-header"><h2 class="mentor-name">John Doe <span class="mentor-rank">#1</span></h2></div>
+                <ul class="mentor-details">
+                    <li><strong>Proficiencies:</strong> Web Development, JavaScript</li>
+                    <li><strong>Experience:</strong> 5 years</li>
+                    <li><strong>Rating:</strong> 7.7(122)</li>
+                    <li><strong>Available Days:</strong> Mon, Wed, Fri</li>
+                    <li><strong>Available Time:</strong> 4:00 PM - 6:00 PM</li>
+                    <li><strong>Fees:</strong> Rs. 300</li>
+                </ul>
+            </div>
+
+            <div class="mentor-card">
+                <div class="mentor-header">
+                <h2 class="mentor-name">Jane Smith <span class="mentor-rank">#2</span></h2>
+                </div>
+                <ul class="mentor-details">
+                <li><strong>Proficiencies:</strong> UI/UX Design, Figma</li>
+                <li><strong>Experience:</strong> 3 years</li>
+                <li><strong>Rating:</strong> 8.3(98)</li>
+                <li><strong>Available Days:</strong> Tue, Thu, Sat</li>
+                <li><strong>Available Time:</strong> 5:00 PM - 7:00 PM</li>
+                <li><strong>Fees:</strong> Rs. 400</li>
+                </ul>
+            </div>
+
+        
+            <div class="mentor-card">
+                <div class="mentor-header">
+                <h2 class="mentor-name">Alice Brown <span class="mentor-rank">#3</span></h2>
+                </div>
+                <ul class="mentor-details">
+                <li><strong>Proficiencies:</strong> Data Science, Python</li>
+                <li><strong>Experience:</strong> 2 years</li>
+                <li><strong>Rating:</strong> 7.4(55)</li>
+                <li><strong>Available Days:</strong> Sun, Wed</li>
+                <li><strong>Available Time:</strong> 3:00 PM - 5:00 PM</li>
+                <li><strong>Fees:</strong> Rs. 250</li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
