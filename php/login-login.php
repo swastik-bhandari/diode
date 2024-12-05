57868f694864601f0bef394bfa0d6345e87d0a0f
swastik-bhandari <swastikbhandari2006@gmail.com>
2024-12-05 10:57:44 +0545

    form validation

--- php/login-login.php
+++ php/login-login.php
@@ -4,21 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Find Your Mentor</title>
-    <link rel="stylesheet" href="login-login-styles.css">
+    <link rel="stylesheet" href="../css/login-login-styles.css">
 </head>
 <body>
-    <form action="" method="POST"></form>
+<?php 
+    $username='';
+    $password='';
+    if($_SERVER['REQUEST_METHOD']==='POST') {
+        function test_input($data) {
+            $data=stripslashes($data);
+            $data=trim($data);
+            $data=htmlspecialchars($data);
+            return $data;
+        }
+
+    $username=test_input($_POST['username']);
+    $password=test_input($_POST['password']);
+}
+    ?>
+
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
     <div id="login-container">
         <div><h2>Login</h2></div>
 
         <div class="input-group">
             <label for="username">Username</label>
-            <input type="text" id="username" placeholder="Enter your username" required>
+            <input type="text" id="username" name ="username" placeholder="Enter your username" required>
         </div>
 
         <div class="input-group">
             <label for="password">Password</label>
-            <input type="password" id="password" placeholder="Enter your password" required>
+            <input type="password" id="password" name ="password" placeholder="Enter your password" required> <span class ="error"> <?php 
+    if(!preg_match('/[A-Z]/',$password) || !preg_match('/[\W_]/' ,$password)) {
+        echo"*atleast one capital letter and one special character";
+    }
+    ?></span>
         </div>
 
         <div class="input-group" id = "showpassword">
@@ -31,7 +51,7 @@
         </div>
 
     </div>
-
-    <script src="login-login-script.js"></script>
+    </form>
+    <script src="../js/login-script.js"></script>
 </body>
 </html>
